b9d797951081ee679a624852fe1472d732e3158c
Ian Schneider <ian@composedcreative.com>
2024-05-17 16:08:21 -0400

    Added header component and style sheet | Added FontAwesome to package.json

--- thrive-workout-frontend/src/App.js
+++ thrive-workout-frontend/src/App.js
@@ -6,14 +6,13 @@ import { useState, useEffect } from 'react'
 import Workouts from './pages/Workouts'
 import AddWorkout from './pages/AddWorkout'
 import EditWorkout from './pages/EditWorkout'
+import Header from './components/header/Header'
 
 function App() {
-  // manage State for exercises and workouts
     const [exercises, setExercises] = useState([])
     const [workouts, setWorkouts] = useState([])
     const navigate = useNavigate()
 
-    // fetch exercises from API/backend when component mounts
     useEffect(() => {
         const loadExercises = async () => {
             const data = await fetchExercises()
@@ -22,33 +21,32 @@ function App() {
         loadExercises()
     }, [])
 
-
-    // function for handling adding a new workout
     const handleAddWorkout = (newWorkout) => {
         setWorkouts([...workouts, newWorkout])
         navigate('/workouts') // Navigate to the workouts page after saving
     }
 
-    // function for handling updating a workout
     const handleUpdateWorkout = (updatedWorkout) => {
-      // update state 
-      // use map to iterate over each workout object in workouts array.  for each workout in the array, if the name of the workout object (workout.name) matches the name of the updated workout (updatedWorkout.name), replace the workout object with the updated workout object, if they dont match then it stays the same 
-      // set new array(created from the map function) as the new state for workouts.
       setWorkouts(workouts.map(workout => workout.name === updatedWorkout.name ? updatedWorkout : workout))
-
-      // navigate to workouts page 
-      // navigate is a function from react-router-dom.  it is used to navigate to a new page.  
       navigate('/workouts')
-    }
+  }
 
-    // function for handling deleting a workout
     const handleDeleteWorkout = (workoutToDelete) => {
         setWorkouts(workouts.filter(workout => workout !== workoutToDelete))
     }
 
+    const handleShowSearch = () => {
+        //show search
+    }
+
     return (
-      // routes for components and passing props
         <div className="App">
+
+            <Header
+                name={ <>THRIVE<span>workout</span></> } 
+                links={["workouts"]}
+                showSearch={handleShowSearch} />
+
             <Routes>
                 <Route path="/" element={<Home exercises={exercises} />} />
                 <Route 
@@ -67,12 +65,9 @@ function App() {
                 />
                 <Route path="/editWorkout/:id" element={<EditWorkout onUpdateWorkout={handleUpdateWorkout} />} />
             </Routes>
+            
         </div>
     )
 }
 
 export default App
-
-
-
-{/* <Route path="/workouts/:id" element={<Home exercises={exercises} />} /> */}
---
+++ thrive-workout-frontend/src/components/header/Header.jsx
@@ -0,0 +1,50 @@
+import {Link} from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass, faCircleUser, faGear } from '@fortawesome/free-solid-svg-icons'
+
+
+import "./Header.scss"
+
+const Header = (props) => {
+    const location = useLocation();
+    return (
+        <header className="tw-main-header">
+            <h1>{props.name}</h1>
+           
+            <div className="tw-main-header--content-right">
+                <nav>
+                {props.links.map( (link, i) => {
+                    
+                    if(location.pathname !== "/"+link){
+                        return(
+                            <Link className="header_nav_item" key={i} to={link}>
+                                {link}
+                            </Link>
+                        )
+                    }else{
+                        return {}
+                    }
+
+
+                })}
+                </nav>
+
+                { location.pathname === "/" && 
+                   <button type='button'>
+                        <FontAwesomeIcon icon={ faMagnifyingGlass } size="2x"  /> 
+                    </button> }
+
+                <div className="tw-main-header--profile-image">
+                <FontAwesomeIcon icon={ faCircleUser } size="4x"  />
+                </div>
+
+                {/* wrap in link to UserSettings */}
+                <FontAwesomeIcon icon={ faGear } size="xl"  />
+            </div>
+                  
+        </header>
+    )
+}
+export default Header
