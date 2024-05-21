7fbe635af6a247bc277b84756e1e2a41f48568fe
Jena <jpantano30@gmail.com>
2024-05-21 14:55:27 -0400

    Imported gsap and added animations to AddWorkout page" q

--- thrive-workout-frontend/src/pages/AddWorkout.jsx
+++ thrive-workout-frontend/src/pages/AddWorkout.jsx
@@ -1,8 +1,13 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react' 
 import fetchExercises from '../services/ExerciseAPI'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faDumbbell, faStopwatch20, faHeartPulse, faPersonRunning, faHeart, faPersonBiking, faHeadphones, faWeight } from "@fortawesome/free-solid-svg-icons" 
 import './styling/AddWorkout.scss'
+import  gsap  from 'gsap' 
+import { useGSAP } from '@gsap/react'
+
+
+
 
 const AddWorkout = ({ onAddWorkout }) => {
     const [workoutName, setWorkoutName] = useState('')
@@ -13,6 +18,14 @@ const AddWorkout = ({ onAddWorkout }) => {
     const [searchTerm, setSearchTerm] = useState('')
 
     const icons = [faDumbbell, faStopwatch20, faHeartPulse, faPersonRunning, faHeart, faPersonBiking, faHeadphones, faWeight]
+
+    
+    // useRef hook to create a reference to elemnts
+    const exerciseSidebarRef = useRef(null) 
+    const exerciseListRef = useRef(null) 
+    const workoutNameRef = useRef(null) 
+
+    // fetch exercises when component mounts
     useEffect(() => {
         const loadExercises = async () => {
             try {
@@ -28,12 +41,14 @@ const AddWorkout = ({ onAddWorkout }) => {
         loadExercises()
     }, [])
 
+    // filter exercises based on search term 
     useEffect(() => {
         setFilteredExercises(
             exercises.filter(exercise => exercise.name.toLowerCase().includes(searchTerm.toLowerCase()))
         )
     }, [exercises, searchTerm])
 
+    // handle changing the selected exercises 
     const handleExerciseChange = (e) => {
         const { value, checked } = e.target
         if (checked) {
@@ -54,9 +69,47 @@ const AddWorkout = ({ onAddWorkout }) => {
         onAddWorkout(newWorkout)
     }
 
+    
+
+    // useGSAP hook to access the gsap animation library
+    useGSAP(() => {
+        // animate sidebar when in view 
+        gsap.fromTo(exerciseSidebarRef.current, {
+            // state before animation
+            opacity: 0,
+            x: 100 // starts 100px to the right of its final position
+        }, {
+            // state after animation
+            opacity: 1,
+            x: 0, // final position 
+            duration: 1, // 1 second
+        });
+
+        // animate exercise list whith staggered effect 
+        gsap.fromTo(exerciseListRef.current.children, {
+            opacity: 0,
+            y: 50 // positioned 50px above its final position
+        }, {
+            opacity: 1,
+            y: 0,
+            duration: 1,
+            stagger: 0.1, // stagger the start time of each child eleemnt by .1 second 
+        }) // cant get the list to animate when user leaves page and comes back ...???
+
+        gsap.fromTo(workoutNameRef.current, {
+            opacity: 0,
+            y: -50 // positioned 50px below its final position
+        }, {
+            opacity: 1,
+            y: 0,
+            duration: 1,
+        })
+    })
+
+
     return ( 
         <div className="addWorkout">
-            <h1 className='h1'>Add Workout</h1>
+            <h1 className='h1' ref={workoutNameRef}>Add Workout</h1>
             <div className='saveWorkout'>
                 <div className='workoutName'>
                     <h3>Workout Name:</h3>
@@ -84,14 +137,14 @@ const AddWorkout = ({ onAddWorkout }) => {
                 </div>
                 <button onClick={handleSave}> Save </button>
             </div>
-            <div className='exerciseSidebar'>
+            <div className='exerciseSidebar' ref={exerciseSidebarRef}>
                 <h3> Select Exercises: </h3>
                 <input 
                     type='text'
                     placeholder='Search Exercises'
                     value={searchTerm}
                     onChange={(e) => setSearchTerm(e.target.value)} />
-                <div className='exerciseList'>
+                <div className='exerciseList' ref={exerciseListRef}>
                     {filteredExercises.length > 0 ? (
                         filteredExercises.map((exercise, index) => (
                             <div key={index}>
